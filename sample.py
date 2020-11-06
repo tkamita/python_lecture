df080bf9c17cc808db7da3b30067dfed1e95b9fc
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-11-06 17:54:07 +0900

    start

---
+++ sample.py
@@ -0,0 +1,31 @@
+num = 1
+name = 'Mike'
+is_ok = True
+
+print(num, type(num))
+print(name, type(name))
+print(is_ok, type(is_ok))
+
+
+
+num = name
+print(num, type(num))
+
+num = 1
+name = '1'
+new_num = int(name)     #型変換
+print(new_num, type(new_num))
+
+print('Hi', 'Mike', sep=',', end='\n')
+
+
+
+# import math
+# result = math.sqrt(25)
+# print(result)
+# y = math.log2(10)
+# print(y)
+
+# print(help(math))
+
+
